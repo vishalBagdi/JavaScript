666dbdd44bf489f9e3354a007d440e7c9ed1e4ed
sainivi931 <sainivi931@gmail.com>
2025-03-08 13:40:46 +0530

    datatypes

---
+++ 01_basic/dataType.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// aleart( 3 + 3) // we are using nodejs, not browser
+
+console.log(3+3) // code readability should be high
+console.log("vishal")
+
+let name = "visal"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => "name"
+// boolean => true/false
+// null => standalone value null is thr representation of empty value
+// undefined =>
+// symbol ==> unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
