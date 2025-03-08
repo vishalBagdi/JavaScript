c18e0d9fba3465d6c0a515031300fb99fcb65e14
sainivi931 <sainivi931@gmail.com>
2025-03-08 15:02:33 +0530

    operations

--- 01_basic/conversionOperations.js
+++ 01_basic/conversionOperations.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "vidhu";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // " " => false
@@ -23,7 +23,25 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber= String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+ 
 
+// ********* operations *****
 
+let value = 3
+ let negValue = -value
+ // console.log(negValue)
+
+ let str1= "hello"
+ let str2= " vishal"
+ let str3= str1+str2
+//console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "3")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
