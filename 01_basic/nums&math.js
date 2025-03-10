1e7a30e0edf12571fb5d85fb93d953c08c8d5620
sainivi931 <sainivi931@gmail.com>
2025-03-10 16:13:07 +0530

    Math&Nums

---
+++ 01_basic/nums&math.js
@@ -0,0 +1,28 @@
+const score = 400;
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(typeof(balance));
+//console.log(balance.toFixed(2)) // toFixed method use when we create e-commarace web
+
+const otherNumber = 23.89864
+//console.log(otherNumber.toPrecision(4))
+const hundreds = 100000;
+//console.log(hundreds.toLocaleString('en-IN'))
+
+// ******* maths ********
+
+//console.log(Math.abs(-5))
+//console.log(Math.round(4.6));// mostly use round method
+//console.log(Math.ceil(4.3)) // returns  the greather value as givern the input
+
+//console.log(Math.random()); 1. Math.random() give the value b/w 0 and 1 .  2. //mostly use this method 
+console.log(Math.floor(Math.random()*1000000) + 1);
+
+const min = 10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
